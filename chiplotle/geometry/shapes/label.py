730eeea2688ab16bf4a80fe10f04859f3fe01fc6
Jim Hanley <jhanley@DGtlRift.com>
2018-01-17 00:12:01 -0500

    Add method for concatination with an existing object.  Does not handle pen color changes

--- chiplotle/geometry/shapes/label.py
+++ chiplotle/geometry/shapes/label.py
@@ -7,3 +7,15 @@ def label(text,
             linespace = None,
             origin = 'bottom-left'):
     return Label(text, charwidth, charheight, charspace, linespace, origin)
+
+def labelCat(label,
+            text,
+            charwidth,
+            charheight,
+            charspace = None,
+            linespace = None,
+            origin = 'bottom-left'):
+    if label is None:
+        return Label(text, charwidth, charheight, charspace, linespace, origin)
+    else:
+	return label.cat(text, charwidth, charheight, charspace, linespace, origin)
